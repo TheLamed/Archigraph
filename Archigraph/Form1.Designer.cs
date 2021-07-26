7adb1dd364fb8c18b7f2026ac37c74cb2e88c78a
Marko Kvyk <markomar2000@gmail.com>
2021-07-26 17:39:30 +0300

    initial

---
+++ Archigraph/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Archigraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbRoot = new System.Windows.Forms.TextBox();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.tbLink = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbRoot
+            // 
+            this.tbRoot.Location = new System.Drawing.Point(12, 55);
+            this.tbRoot.Name = "tbRoot";
+            this.tbRoot.ReadOnly = true;
+            this.tbRoot.Size = new System.Drawing.Size(260, 23);
+            this.tbRoot.TabIndex = 0;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(12, 12);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(260, 37);
+            this.btnChoose.TabIndex = 1;
+            this.btnChoose.Text = "Choose .zip";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(12, 139);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(260, 37);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create Telegra.ph";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.AutoSize = true;
+            this.lbInfo.Location = new System.Drawing.Point(12, 179);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(0, 15);
+            this.lbInfo.TabIndex = 3;
+            // 
+            // tbLink
+            // 
+            this.tbLink.Location = new System.Drawing.Point(12, 225);
+            this.tbLink.Name = "tbLink";
+            this.tbLink.ReadOnly = true;
+            this.tbLink.Size = new System.Drawing.Size(260, 23);
+            this.tbLink.TabIndex = 4;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(12, 110);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(260, 23);
+            this.tbTitle.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Title";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.tbLink);
+            this.Controls.Add(this.lbInfo);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.tbRoot);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Archigraph";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRoot;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label lbInfo;
+        private System.Windows.Forms.TextBox tbLink;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Archigraph/Form1.cs
@@ -0,0 +1,147 @@
+﻿using Kvyk.Telegraph;
+using Kvyk.Telegraph.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Archigraph
+{
+    public partial class Form1 : Form
+    {
+        public string ArchiveFilePath { get; set; }
+        public bool IsLoading { get; set; } = false; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            if (IsLoading)
+                return;
+
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                if (ArchiveFilePath != null)
+                    openFileDialog.InitialDirectory = Path.GetFullPath(ArchiveFilePath);
+
+                openFileDialog.Filter = "Files|*.zip;";
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    ArchiveFilePath = openFileDialog.FileName;
+                    tbRoot.Text = ArchiveFilePath;
+                }
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (IsLoading)
+                return;
+
+            btnCreate_Click();
+        }
+        private async Task btnCreate_Click()
+        {
+            IsLoading = true;
+
+            try
+            {
+                if (string.IsNullOrEmpty(ArchiveFilePath))
+                {
+                    MessageBox.Show("Select archive", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(tbTitle.Text))
+                {
+                    MessageBox.Show("Enter Title", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                lbInfo.Text = $"Connecting to Telegra.ph";
+
+                var client = new TelegraphClient();
+                client.AccessToken = (await client.CreateAccount("Archigraph")).AccessToken;
+
+
+                using ZipArchive archive = ZipFile.OpenRead(ArchiveFilePath);
+                int i = 0;
+                var nodes = new List<Node>();
+
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    lbInfo.Text = $"Loading: {i + 1} from {archive.Entries.Count}";
+
+                    var mime = GetMIMETypeByName(entry.FullName);
+                    if (mime != "application/octet-stream")
+                    {
+                        var s = entry.Open();
+                        var ms = new MemoryStream();
+                        await s.CopyToAsync(ms);
+                        var arr = ms.ToArray();
+                        s.Dispose();
+
+                        var file = await client.UploadFile(new FileToUpload { Bytes = arr, Type = mime });
+                        nodes.Add(Node.ImageFigure(file.Path));
+                    }
+                    i++;
+                }
+
+                lbInfo.Text = $"Creating Telegra.ph page";
+
+
+                var page = await client.CreatePage(tbTitle.Text, nodes);
+
+                tbLink.Text = page.Url;
+
+                lbInfo.Text = $"Done!";
+
+                var psInfo = new ProcessStartInfo
+                {
+                    FileName = page.Url,
+                    UseShellExecute = true
+                };
+                Process.Start(psInfo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if(lbInfo.Text != "Done!")
+                    lbInfo.Text = "";
+
+                IsLoading = false;
+            }
+        }
+
+        private string GetMIMETypeByName(string name)
+        {
+            var ext = Path.GetExtension(name);
+            switch (ext)
+            {
+                case ".png": return "image/png";
+                case ".jpeg":
+                case ".jpg": return "image/jpg";
+                case ".gif": return "image/gif";
+                default:
+                    return "application/octet-stream";
+            }
+        }
+
+    }
+}
